c6184fef9d0684e0b300802389ff8ecefe74f8ca
Jan Kala <jan.kala@jamf.com>
2022-05-19 13:45:13 +0200

    update of the browser ectension

--- BrowsersSpecific/Firefox/background.js
+++ BrowsersSpecific/Firefox/background.js
@@ -2,21 +2,36 @@ let port = null;
 
 function sendDataToSysProcess(details){
     console.log(details)
-    message = {message: details}
-    port.postMessage(message);
+
+    // message = {message: details}
+    // port.postMessage(message);
 }
+
+async function logSubject(details) {
+    try {
+      let securityInfo = await browser.webRequest.getSecurityInfo(details.requestId, {});
+      message = {data: details, secInfo: securityInfo}
+      console.log(message);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
 
 const onNativeMessageReceived = (message) => {
     // console.log(message)
 }
 
-chrome.webRequest.onSendHeaders.addListener(sendDataToSysProcess,
-    {urls: ["<all_urls>"]}
-);
-chrome.webRequest.onCompleted.addListener(sendDataToSysProcess,
-    {urls: ["<all_urls>"]}
+// chrome.webRequest.onSendHeaders.addListener(
+//     sendDataToSysProcess,
+//     {urls: ["<all_urls>"]}
+// );
+browser.webRequest.onHeadersReceived.addListener(
+    logSubject,
+    {urls: ["<all_urls>"]},
+    ["blocking"]
 );
 
 // Connect to the Native Message Host
-port = chrome.runtime.connectNative("com.kala.annotator");
-port.onMessage.addListener(onNativeMessageReceived)
+// port = chrome.runtime.connectNative("com.kala.annotator");
+// port.onMessage.addListener(onNativeMessageReceived)
--- background.js
+++ background.js
@@ -1,22 +1,96 @@
-let port = null;
+let nativeMessagingPort = null;
+let requests = {}
 
-function sendDataToSysProcess(details){
-    console.log(details)
-    message = {message: details}
-    port.postMessage(message);
+function onSendHeadersCallback(details){
+    // collect request data and save it under requestId key
+    storageKey = details.requestId
+    requests[storageKey] = details;
+
+
 }
 
+function onResponseStartedCallback(details){
+    // cached content is not travelling through network
+    if (details.fromCache == true){
+        return;
+    }
+
+    url = new URL(details.url)
+
+    // We don't care about other protocols
+    if (url.protocol != "https:" && url.protocol != "http:"){
+        return;
+    }
+
+    // in case of standart ports, field is empty
+    let port;
+    if (url.port == ''){
+        if (url.protocol == "http:"){
+            port = 80;
+        } else if (url.protocol == "https:"){
+            port = 443;
+        }
+    }
+
+    // decide what IP version is being used
+    let ip_version = 4;
+    if (details.ip.includes(":")){
+        ip_version = 6;
+    }
+
+    // Timestamp is in miliseconds and we need microseconds
+    timeStamp = Math.trunc(details.timeStamp * 1000);
+
+    storageKey = details.requestId;
+
+    // Construct message
+    message = {
+        trigger: "onResponseStarted",
+        hostname: url.hostname,
+        proto: url.protocol,
+        ipVersion: ip_version,
+        ip: details.ip,
+        port: port,
+        timeStamp_s: timeStamp,
+        timeStamp_ms: 0,
+        details: {
+            request: requests[storageKey],
+            response: details
+        }
+
+    }
+
+    // Log and send
+    console.log(message)
+    nativeMessagingPort.postMessage(message);
+}
+
+function errorOccured(details){
+    message = {trigger: "error", details: details}
+    console.log(message)
+}
+// communication backwards is not supported
 const onNativeMessageReceived = (message) => {
     // console.log(message)
 }
 
-chrome.webRequest.onSendHeaders.addListener(sendDataToSysProcess,
-    {urls: ["<all_urls>"]}
+chrome.webRequest.onSendHeaders.addListener(
+    onSendHeadersCallback,
+    {urls: ["<all_urls>"]},
+    ["requestHeaders"]
 );
-chrome.webRequest.onCompleted.addListener(sendDataToSysProcess,
+
+chrome.webRequest.onResponseStarted.addListener(
+    onResponseStartedCallback,
+    {urls: ["<all_urls>"]},
+    ["responseHeaders"]
+);
+chrome.webRequest.onErrorOccurred.addListener(
+    errorOccured,
     {urls: ["<all_urls>"]}
 );
 
 // Connect to the Native Message Host
-port = chrome.runtime.connectNative("com.kala.anotator");
-port.onMessage.addListener(onNativeMessageReceived)
+nativeMessagingPort = chrome.runtime.connectNative("com.kala.anotator");
+nativeMessagingPort.onMessage.addListener(onNativeMessageReceived)
+
